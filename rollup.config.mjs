006898dfb2d58f9e4f1fc8d68388604e617d0ab3
Jo Shinonome <jo.shinonome@gmail.com>
2023-07-20 23:12:49 +0800

    update deps

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -6,7 +6,8 @@
  */
 
 import { babel } from '@rollup/plugin-babel';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
+
 export default {
   input: 'src/index.js',
   output: {
